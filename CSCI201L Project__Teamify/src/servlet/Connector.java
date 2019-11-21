886ccde68c92b9fda4f002803b7810fe696bcde8
MikGh-29 <zhuohang@usc.edu>
2019-11-20 23:03:29 -0800

    Update JDBC to search user and project

--- CSCI201L Project__Teamify/src/servlet/Connector.java
+++ CSCI201L Project__Teamify/src/servlet/Connector.java
@@ -23,28 +23,65 @@ public class Connector {
 	public String verifyUser(String username, String password) {
 		String response = null;
 		String cmd = "SELECT * FROM Project.Users WHERE Username=?;";
-		ps = con.prepareStatement(cmd);
-		ps.setString(1, username);
-		rs = ps.executeQuery();
-		String temp = null;
-		if(rs.next()) temp = rs.getString("Password");
-		if(temp == null) response = "No user has been found";
-		else if(!temp.equals(password)) response = "Incorrect password";
-		else temp = "Success";
+		try {
+			ps = con.prepareStatement(cmd);
+			ps.setString(1, username);
+			rs = ps.executeQuery();
+			String temp = null;
+			if(rs.next()) temp = rs.getString("Password");
+			if(temp == null) response = "No user has been found";
+			else if(!temp.equals(password)) response = "Incorrect password";
+			else temp = "Success";
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
 		return response;
 	}
 	
-	public static List<T> getByTag(Class<T> type, List<String> tags) {
-		
-		if(type.isInstance(new Project())） {
-			
+	public List<String> findProject(String term) {
+		String cmd = "SELECT * FROM Project.Projects WHERE LOWER(ProjectName)=LOWER(?);";
+		List<String> projects = new ArrayList<String>();
+		try {
+			ps = con.prepareStatement();
+			ps.setString(1, term);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				projects.add(rs.getString("ProjectName"));
+			}
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
 		}
-		String cmd = "SELECT t.? FROM Tags t WHERE t.?=?;";
+		return projects;
+	}
+	
+	public List<String> findUser(String term) {
+		String cmd = "SELECT * FROM Project.Users WHERE LOWER(Username)=LOWER(?);";
+		List<String> users = new ArrayList<String>();
+		try {
+			ps = con.prepareStatement();
+			ps.setString(1, term);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				projects.add(rs.getString("Username"));
+			}
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return users;
+	}
+	
+	public static <T, E> List<T> getByTag(Class<T> type, Class<E> altType, List<String> tags) {
+		String name = type == Project.class ? "ProjectName" : "Username";
+		String targetName = name.equals("ProjectName") ? "Username" : "ProjectName";
+		String table = name.equals("ProjectName") ? "Projects" : "Users";
+		
+		String cmd = "SELECT t.? FROM Tags t WHERE t.TagName=?;";
 		ps = con.prepareStatement(cmd);
 		Map<String, int> count = new HashMap<String, int>();
 		List<T> target = new ArrayList<T>();
 		for(String tag : tags) {
-			ps.setString(1, tag);
+			ps.setString(1, name);
+			ps.setString(2, tag);
 			rs = ps.executeQuery();
 			while(rs.next()) {
 				String temp;
@@ -59,29 +96,32 @@ public class Connector {
 			.sorted(Entry.comparingByValue())
 			.collect(Collectors.toMap(Entry::getKey, Entry::getValue,
             (e1, e2) -> e1, LinkedHashMap::new));
-		cmd = "SELECT * FROM Projects WHERE ProjectName=?;";
+			
+		cmd = "SELECT * FROM ? WHERE ?=?;";
 		ps = con.prepareStatement();
+		ps.setString(1, table);
+		ps.setString(2, name);
 		Iterator it = sorted.entrySet().iterator();
 		while(it.hasNext()) {
 			Map.Entry me = (Map.Entry)it.next();
-			ps.setString(1, (String)it.getKey());
+			ps.setString(3, (String)it.getKey());
 			rs = ps.executeQuery();
-			projects.add(new Project(rs.getString("ProjectName"), rs.getString("Description")));
+			target.add(T.getDeclaredConstructor(String.class, String.class).newInstance(rs.getString("ProjectName"), rs.getString("Description")));
 		}
-		cmd = "SELECT * FROM Project.ProjectMembers WHERE ProjectName=?;";
-		ps = con.prepareStatement();
-		for(Project p : projects) {
-			ps.setString(1, p.name);
-			rs = ps.executeQuery();
-			List<String> users = new ArrayList<String>();
-			while(rs.next()) user.add(rs.getString("Username"));
-			p.setUsers(users);
-		}
-		return projects;
-	}
-	
-	public List<User> getUserByTag(List<String> tags) {
 		
+		cmd = "SELECT * FROM ?.ProjectMembers WHERE ?=?;";
+		ps = con.prepareStatement();
+		ps.setString(1, table);
+		ps.setString(2, name);
+		for(T p : target) {
+			ps.setString(3, p.name);
+			rs = ps.executeQuery();
+			List<String> alt = new ArrayList<String>();
+			while(rs.next()) alt.add(rs.getString(targetName));
+			if(type == Project.class) p.setUsers(alt);
+			else p.setProjects(alt);
+		}
+		return target;
 	}
 	
 }
