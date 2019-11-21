7598ab5ec32f14409ee6c00fff06c4f5ca166ff1
MikGh-29 <zhuohang@usc.edu>
2019-11-20 23:05:11 -0800

    Project and User classes

---
+++ CSCI201L Project__Teamify/src/servlet/Project.java
@@ -0,0 +1,21 @@
+
+public class Project {
+	
+	protected String name;
+	protected String description;
+	protected List<String> users = null;
+	
+	public Project(String n, String d) {
+		name = n;
+		description = d;
+	}
+	
+	public void setUser(List<String> u) {
+		users = u;
+	}
+	
+	public String getName() { return name; }
+	public String getDescription() { return description; }
+	public List<String> getUser() { return users; }
+	
+}
