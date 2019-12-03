73873abf9bf5a9cb27adfaaf4adc263d33f277ec
Zhuohang Liu <zhuohang@usc.edu>
2019-12-02 18:46:25 -0800

    Database script update

--- CSCI201L Project__Teamify/build/classes/servlet/init.sql
+++ CSCI201L Project__Teamify/build/classes/servlet/init.sql
@@ -8,20 +8,21 @@ CREATE TABLE Users (
 );
 CREATE TABLE Projects (
 	ProjectName varchar(100) PRIMARY KEY NOT NULL,
-	Description varchar(2000) NOT NULL
+	Description varchar(2000) NOT NULL,
+	Image varchar(200) NOT NULL
 );
 CREATE TABLE Tags (
 	TagID int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	TagName varchar(20) NOT NULL,
 	Username varchar(50),
 	ProjectName varchar(100),
-	FOREIGN KEY Username REFERENCES Users(Username),
-	FOREIGN KEY ProjectName REFERENCES Projects(ProjectName)
+	FOREIGN KEY (Username) REFERENCES Users(Username),
+	FOREIGN KEY (ProjectName) REFERENCES Projects(ProjectName)
 );
 CREATE TABLE ProjectMembers (
 	PMID int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	ProjectName varchar(100) NOT NULL,
 	Username varchar(50) NOT NULL,
-	FOREIGN KEY ProjectName REFERENCES Projects(ProjectName),
-	FOREIGN KEY Username REFERENCES Users(Username)
+	FOREIGN KEY (ProjectName) REFERENCES Projects(ProjectName),
+	FOREIGN KEY (Username) REFERENCES Users(Username)
 );
