b4997ab7c1a2a14614787162d2366378c18f4d92
Zhuohang Liu <zhuohang@usc.edu>
2019-12-02 19:19:54 -0800

    Update ProfileServ

--- CSCI201L Project__Teamify/src/servlet/ProfileServ.java
+++ CSCI201L Project__Teamify/src/servlet/ProfileServ.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,7 +28,9 @@ public class ProfileServ extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		Connector con = (Connector)request.getSession().getAttribute("Connector");
+		String name = (String)request.getSession().getAttribute("name");
+		List<String> info = con.getUserData(name);
 	}
 
 }
